9fd5be26827dddaecdfc60277dd3dbb65dffc5d0
amkera <amber_kerawala@berkeley.edu>
2021-02-10 19:05:36 -0500

    added minimum skew

---
+++ minimum_skew.py
@@ -0,0 +1,3 @@
+# Input:  A DNA string Genome
+# Output: A list containing all integers
+#i minimizing Skew(Prefix_i(Text)) over all values of i (from 0 to |Genome|)
--- skew_array.py
+++
@@ -1,8 +0,0 @@
-def SkewArray(Genome):
-    skew = [0]
-    score = {"A":0, "T":0, "C":-1, "G":1}
-    for i in range(1,len(Genome)+1):
-            skew.append(score[Genome[i-1]] + skew[i-1])
-            print("This is score[Genome[i-1]]: " + str(score[Genome[i-1]]))
-            print(skew[i-1])
-    return skew
