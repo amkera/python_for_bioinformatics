9e4a4deb51ca7224c14678627de5cd44edf31ed5
amkera <amber_kerawala@berkeley.edu>
2021-02-17 19:22:22 -0500

    padding test case

--- count_motifs.py
+++ count_motifs.py
@@ -1,1 +1,21 @@
 #inputs are the motifs, a list of kmers (strings)
+#output the count matrix of motifs as a dictionary of lists
+
+
+
+
+
+
+
+
+
+
+
+
+print(Count([
+    "AACGTG",
+    "GTGCAC",
+    "GTGCGT",
+    "CACGTG",
+    "CCCGGT"
+]))
