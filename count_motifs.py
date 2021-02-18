c9f7f7baeb499484d56c0294be9abf9734dbab1e
amkera <amber_kerawala@berkeley.edu>
2021-02-17 19:23:32 -0500

    setting up variables

--- count_motifs.py
+++ count_motifs.py
@@ -1,7 +1,9 @@
 #inputs are the motifs, a list of kmers (strings)
 #output the count matrix of motifs as a dictionary of lists
 
-
+def Count(Motifs):
+    count = {} #final dictionary
+    k = len(Motifs[0]) #6 
 
 
 
