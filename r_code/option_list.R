55d11792e4f79c061ea86b4a548b6b190446b5ac
amkera <amber_kerawala@berkeley.edu>
2021-03-11 18:26:00 -0500

    optparse code in R:

--- r_code/option_list.R
+++ r_code/option_list.R
@@ -1,4 +1,8 @@
 option_list = list(
   make_option(c("--sanger"), default=NULL, type='character',
               help="enter the folder where the sanger data lives"),
-  
+  make_option(c("--freq), default=NULL, type='character',
+              help="enter the folder where the NGS frequencies live"),
+
+)
+opt = parse_args(OptionParser(option_list=option_list))
