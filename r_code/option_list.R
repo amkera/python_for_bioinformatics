27903844cb5c1b8c27e5f7285eaaab15080044e8
amkera <amber_kerawala@berkeley.edu>
2021-03-11 18:27:21 -0500

    added outfile

--- r_code/option_list.R
+++ r_code/option_list.R
@@ -3,6 +3,7 @@ option_list = list(
               help="enter the folder where the sanger data lives"),
   make_option(c("--freq), default=NULL, type='character',
               help="enter the folder where the NGS frequencies live"),
+  make_option(c("outfile"), default=NULL,
+              help="Enter intended name of output csv file")
 
 )
-opt = parse_args(OptionParser(option_list=option_list))
